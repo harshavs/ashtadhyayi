6ad4409639fb87884cc9bd2a82edb6f0e9d4b06b
Harsha Veeravalli <harshavs@gmail.com>
2020-07-16 23:03:39 -0400

    कृमृदृरुहिभ्यश्छन्दसि_३_१_५९

--- tests/test_tinantarupam.py
+++ tests/test_tinantarupam.py
@@ -6,7 +6,8 @@ from ashtadhyayi.lasya import लकारः
 class TestTinantarupam(unittest.TestCase):
 
     def test_तिङन्तरूपम्_करोति(self):
-        करोति = तिङन्तम्('डुकृ॒ञ्', लकारः.लोट्, True, 0, 0)
+        धातुः = {'धातुः': 'डुकृ॒ञ्', 'गणः': 'तनादिः'}
+        करोति = तिङन्तम्(धातुः, लकारः.लोट्, True, 0, 0)
         self.assertTrue(करोति.तिङ्_प्रत्ययः['सार्वधातुकम्'])
         self.assertEqual(करोति.तिङ्_प्रत्ययः['प्रत्ययः'], 'तुप्')
 
